eaf358cd6825e294110a603ee54ca58b3af235e3
aitalibraham <farouk.alibraham@gmail.com>
2020-03-05 15:46:27 +0100

    Adding factory patern for creating parsers, feature 1

--- Projet/src/main/java/parser/ParserFactory.java
+++ Projet/src/main/java/parser/ParserFactory.java
@@ -2,12 +2,21 @@ package parser;
 
 public class ParserFactory {
 
-	private ParserFactory() {
-		
-	}
+	/**
+	 * ParserFactory cannot be instantiated
+	 */
+	private ParserFactory() {}
+	
+	/**
+	 * 
+	 * @param filePath : file path of the file to parse, not null
+	 * @return FileParser : the right FileParser
+	 */
 	public static FileParser createParser(String filePath) {
 		if (filePath.substring(filePath.lastIndexOf('.') + 1).equalsIgnoreCase("CSV"))
 			return new CsvParser(filePath);
+		else if (filePath.substring(filePath.lastIndexOf('.') + 1).equalsIgnoreCase("json"))
+			return new JsonParser(filePath);
 		return null;
 	}
 }
